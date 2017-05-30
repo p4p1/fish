b3b85a42515e660a8dc9a8e4597d82590ec3d63b
Leo Smith <k0oo@icloud.com>
2017-05-30 23:21:32 +0200

    🎣

---
+++ setup.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+cp * /Library/WebServer/Documents/fish/
+cp admin/* /Library/WebServer/Documents/fish/admin/
+cp cgi-bin/* /Library/WebServer/Documents/fish/cgi-bin/
+cp data/* /Library/WebServer/Documents/fish/data/
+cp fish_logic/* /Library/WebServer/Documents/fish/fish_logic/
